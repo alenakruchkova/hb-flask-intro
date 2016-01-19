8ed3fe8edf0a52c49efbcac5177f1380cb64d775
Hackbright Student <info@hackbrightacademy.com>
2016-01-19 12:42:22 -0800

    link to /hello, dropdown menu at /hello, and display drop down selection in /greet

---
+++ nice.py
@@ -0,0 +1,87 @@
+from random import choice
+
+from flask import Flask, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Hi! This is the home page.</h1>
+        <p><a href=http://localhost:5000/hello>Click for a greeting!</a></p>
+      </body> 
+    </html>
+    """
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          <label for= "compliment">Compliment:</label>
+            <select name="self-compliment" id="compliment">
+              <option value="pretty">Pretty</option>
+              <option value="pretty and smart">Pretty & Smart</option>
+              <option value="just smart">Just Smart</option>
+            </select>
+          <br>
+
+          <label>What's your name? <input type="text" name="person"></label>
+          <input type="submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    nice_word = request.args.get("self-compliment")
+
+    compliment = choice(AWESOMENESS)
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi %s, you are %s and I think you're %s!
+      </body>
+    </html>
+    """ % (player,nice_word, compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
